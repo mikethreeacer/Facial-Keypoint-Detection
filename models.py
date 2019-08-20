a5878e623bc08404a7e9a4f282f807f520fe792f
Cheng Tsung Yu <mikethreeacer@gmail.com>
2019-08-20 21:23:48 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -19,8 +19,25 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        #224*224 input image
+        self.conv1 = nn.Conv2d(1, 32, 5)   #220
+        self.conv2 = nn.Conv2d(32, 64, 4)  #107
+        self.conv3 = nn.Conv2d(64, 128, 3) #51
+        self.conv4 = nn.Conv2d(128, 256, 2) #24   last is 12*12
         
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.dropout1 = nn.Dropout(0.1)
+        self.dropout2 = nn.Dropout(0.2)
+        self.dropout3 = nn.Dropout(0.3)
+        self.dropout4 = nn.Dropout(0.4)
+        self.dropout5 = nn.Dropout(0.5)
+        self.dropout6 = nn.Dropout(0.6)
+        
+        
+        self.fc1 = nn.Linear(12*12*256, 1000)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc3 = nn.Linear(1000, 136)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -30,7 +47,19 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.dropout3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.dropout4(x)
+        x = x.view(-1, 12*12*256)
+        x = F.relu(self.fc1(x))
+        x = self.dropout5(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout6(x)
+        x = F.relu(self.fc3(x))
         # a modified x, having gone through all the layers of your model, should be returned
         return x
